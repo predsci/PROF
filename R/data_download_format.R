a38cc87ed54c37cb53138aa8a8d501d795df6f93
mbennun <mbennun@predsci.com>
2023-11-10 10:27:07 -0800

    mbn: updated documentation and enabled more flexibility by allowing the user to independently select the start/end dates for fitting of the mechanical and statistical models.  By default both are set to the entire data stream

--- R/data_download_format.R
+++ R/data_download_format.R
@@ -330,8 +330,8 @@ hhs_data_ex <- function(season = NULL, state=NULL, fit_end = NULL) {
 #' Convert HHS hospitalization data to PROF format.
 #'
 #' This function converts HHS hospitalization data to the structure that
-#' works with with PROF fitting routines.  This function retrieves data 
-#' for an entire season and expects the HHS data is already downloaded. 
+#' works with with PROF fitting routines.  This function retrieves data
+#' for an entire season and expects the HHS data is already downloaded.
 #' Before sending to fitting procedures, the output of this function
 #' needs fitting date ranges (see PROF::hhs_set_fitdates()).
 #'
@@ -344,48 +344,48 @@ hhs_data_ex <- function(season = NULL, state=NULL, fit_end = NULL) {
 #'
 #'
 hhs_2_PROF <- function(hhs_path=NULL, season = NULL, state=NULL) {
-  
+
   if(is.null(state)) {
     cat("\nState Abbbreviation Not Provided, Defaulting to CA\n")
     state = 'CA'
   }
-  
+
   if (is.null(season)) {
     cat("\nSeason Not Provided, Defaulting to 2022-2023")
     season = 2022
   }
-  
+
   # check that we support the requested season
   supported_seasons = c(2021, 2022, 2023)
-  
+
   if (!any(supported_seasons == season)) stop('\nRequested Season is Not Supported\n')
-  
+
   if (is.null(hhs_path)) {
-    stop("User must specify a path to the HHS data CSV file.",  
+    stop("User must specify a path to the HHS data CSV file.",
          "  See PROF::hhs_hosp_state_down() to download current data file.")
   }
   # load the file
   hosp_data = load_HHS_csv(hhs_file=hhs_path)
-  
+
   # --- Subset influenza admits data for Requested State ---
   # Set observed data subset dates
-  
+
   start_date = as.Date(paste0(season,'-09-01'))
   end_date   = as.Date(paste0(season+1,'-06-01'))
-  
+
   if (season == 2023) {
     start_date = as.Date(paste0(season,'-07-01'))
   }
-  
+
   keep_cols = c("previous_day_admission_influenza_confirmed",
                 "previous_day_admission_influenza_confirmed_coverage",
                 "previous_day_deaths_influenza",
                 "previous_day_deaths_influenza_coverage")
-  
+
   state_inf = get_HHS_state(hosp_data=hosp_data, get_cols=keep_cols,
                             state=state, start_date=start_date,
                             end_date=end_date)
-  
+
   # format the data for fitting
   fit_col = "previous_day_admission_influenza_confirmed"
   inc_type = "hosp_admits"
@@ -393,22 +393,22 @@ hhs_2_PROF <- function(hhs_path=NULL, season = NULL, state=NULL) {
   population = get_loc_pop(location=state)
   flu_data = format_hhs_state(state_data=state_inf, fit_col=fit_col, loc_name=state,
                               pop=population, disease=disease, inc_type=inc_type)
-  
+
 
   # --- Repeat for COVID ---
   start_date = as.Date(paste0(season,'-10-15'))
   end_date   = as.Date(paste0(season+1,'-06-01'))
-  
+
   if (season == 2023) {
     start_date = as.Date(paste0(season,'-06-01'))
   }
-  
+
   keep_cols = c("previous_day_admission_adult_covid_confirmed",
                 "previous_day_admission_adult_covid_confirmed_coverage",
                 "previous_day_admission_pediatric_covid_confirmed",
                 "previous_day_admission_pediatric_covid_confirmed_coverage",
                 "deaths_covid", "deaths_covid_coverage")
-  
+
   state_cov = get_HHS_state(hosp_data=hosp_data, get_cols=keep_cols,
                             state=state, start_date=start_date,
                             end_date=end_date)
@@ -416,7 +416,7 @@ hhs_2_PROF <- function(hhs_path=NULL, season = NULL, state=NULL) {
   state_cov[["previous_day_admission_confirmed"]] =
     state_cov$previous_day_admission_adult_covid_confirmed +
     state_cov$previous_day_admission_pediatric_covid_confirmed
-  
+
   # format the data for fitting
   fit_col = "previous_day_admission_confirmed"
   inc_type = "hosp_admits"
@@ -424,18 +424,18 @@ hhs_2_PROF <- function(hhs_path=NULL, season = NULL, state=NULL) {
   population = get_loc_pop(location=state)
   cov_data = format_hhs_state(state_data=state_cov, fit_col=fit_col, loc_name=state,
                               pop=population, disease=disease, inc_type=inc_type)
-  
+
   # --- Combine two diseases into a single data structure ---
   prof_data = list(covid19=cov_data$covid19, influenza=flu_data$influenza)
-  
+
   return(prof_data)
 }
 
 
 #' Add fit-data structures to PROF_data
-#' 
+#'
 #' Fit-data structures explicitly state the data to be passed to the fitting
-#' routine.  This allows the user to exclude data from the begining or end 
+#' routine.  This allows the user to exclude data from the begining or end
 #' of the season in the model fit.
 #' @param prof_data list. Generally the output from PROF::hhs_2_PROF().
 #' @param fit_start Date. The lower limit (inclusive) of the data range to
@@ -443,19 +443,19 @@ hhs_2_PROF <- function(hhs_path=NULL, season = NULL, state=NULL) {
 #' @param fit_end Date. The upper limit (inclusive) of the data range to
 #' be fit.
 #'
-#' @return PROF_data structure that now includes a $data_fit entry for 
+#' @return PROF_data structure that now includes a $data_fit entry for
 #' each pathogen.
 #' @export
 #'
 #'
 hhs_set_fitdates <- function(prof_data=NULL, fit_start=NULL, fit_end=NULL) {
-  
+
   # determine all pathogens
   pathogens = names(prof_data)
-  
+
   for (pathog in pathogens) {
     if(!is.null(fit_end)) {
-      if (!(fit_end<=max(prof_data[[pathog]]$data$date) && 
+      if (!(fit_end<=max(prof_data[[pathog]]$data$date) &&
           fit_end>=min(prof_data[[pathog]]$data$date))) {
         stop("\nRequested fit_end is NOT consistent with selected season\n")
       }
@@ -463,20 +463,69 @@ hhs_set_fitdates <- function(prof_data=NULL, fit_start=NULL, fit_end=NULL) {
     } else {
       fit_end_use = max(max(prof_data[[pathog]]$data$date))
     }
-    
+
     if (is.null(fit_start)) {
       fit_start_use = min(prof_data[[pathog]]$data$date)
     } else {
       fit_start_use = fit_start
     }
-    
+
     # set fit data entry
     prof_data[[pathog]]$data_fit = prof_data[[pathog]]$data[
       prof_data[[pathog]]$data$date >= fit_start_use &
         prof_data[[pathog]]$data$date <= fit_end_use,
     ]
   }
-  
+
+  return(prof_data)
+}
+
+
+#' Add fit-data-stat structures to PROF_data
+#'
+#' Fit-data-stat structures explicitly state the data to be passed to the statistical
+#' fitting routine.  This allows the user to exclude data from the beginning or end
+#' of the season in the model fit.
+#' @param prof_data list. Generally the output from PROF::hhs_2_PROF().
+#' @param fit_start Date. The lower limit (inclusive) of the data range to
+#' be fit.
+#' @param fit_end Date. The upper limit (inclusive) of the data range to
+#' be fit.
+#'
+#' @return PROF_data structure that now includes a $data_fit_stat entry for
+#' each pathogen.
+#' @export
+#'
+#'
+hhs_set_fitdates_stat <- function(prof_data=NULL, fit_start=NULL, fit_end=NULL) {
+
+  # determine all pathogens
+  pathogens = names(prof_data)
+
+  for (pathog in pathogens) {
+    if(!is.null(fit_end)) {
+      if (!(fit_end<=max(prof_data[[pathog]]$data$date) &&
+            fit_end>=min(prof_data[[pathog]]$data$date))) {
+        stop("\nRequested fit_end is NOT consistent with selected season\n")
+      }
+      fit_end_use = fit_end
+    } else {
+      fit_end_use = max(max(prof_data[[pathog]]$data$date))
+    }
+
+    if (is.null(fit_start)) {
+      fit_start_use = min(prof_data[[pathog]]$data$date)
+    } else {
+      fit_start_use = fit_start
+    }
+
+    # set fit data entry
+    prof_data[[pathog]]$data_fit_stat = prof_data[[pathog]]$data[
+      prof_data[[pathog]]$data$date >= fit_start_use &
+        prof_data[[pathog]]$data$date <= fit_end_use,
+    ]
+  }
+
   return(prof_data)
 }
 
